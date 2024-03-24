b985d68083677ddbe62eabaaaabaad0f0f8ebef4
Andy Pandy <andytgamedev@gmail.com>
2024-03-24 14:38:29 +0200

    started on interaction

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,9 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using Unity.VisualScripting;
+using UnityEditor.Search;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+
     Vector2 turn;
 
     public float speed = 50f;
@@ -12,16 +16,45 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] float lockedHeight = 0;
     Vector3 direction = Vector3.zero;
 
+    int correctQuestions = 0;
+    bool firstInteraction = true;
+    int lastInteractedExhibitIndex = -1;
+
+    Question[] questions;
+    Question currentQuestion;
+
+
+
     // Start is called before the first frame update
     void Start()
     {
         // Lock the cursor inside the window
         Cursor.lockState = CursorLockMode.Locked;
+        makeAllQuestions();
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            RaycastHit rayInfo;
+            if (Physics.Raycast(transform.position, transform.forward, out rayInfo, 2.0f))
+            {
+                if (rayInfo.transform.CompareTag("Exhibit"))
+                {
+                    // store the painting info for the question
+                    handleExhibitInteraction();
+                } 
+                else if (rayInfo.transform.CompareTag("Door"))
+                {
+                    // if first time, show message
+                    // if not first time, compare stored painting, show question etc
+                    handleDoorInteraction();
+                }
+            }
+        }
 
         float horizontal = Input.GetAxisRaw("KB Horizontal");
         float vertical = Input.GetAxisRaw("KB Vertical");
@@ -41,4 +74,47 @@ public class PlayerMovement : MonoBehaviour
 
     }
 
+    struct Question
+    {
+        public string question { get; }
+        public int[] correctExhibitIndexes { get; }
+
+        public Question(string q, int[] i ) : this()
+        {
+            question = q;
+            correctExhibitIndexes = i;
+        }
+    }
+
+    void makeAllQuestions()
+    {
+        questions[0] = new Question("find a painting with a kokori", new int[] { 9 });
+    }
+
+    void handleExhibitInteraction()
+    {
+
+    }
+
+    void handleDoorInteraction()
+    {
+        if (firstInteraction)
+        {
+            // show ui and first question
+            firstInteraction = false;
+        }
+
+        if (currentQuestion.correctExhibitIndexes.Contains(lastInteractedExhibitIndex))
+        {
+            correctQuestions++;
+            // set next question by changing currentQuestion
+        }
+
+        if (correctQuestions >= 10)
+        {
+            // show exit ui and exit
+        }
+
+        // show question info etc
+    }
 }
